2217126c0a307c5e9e4df0229b93ab8173f9d447
Anusha G M <anusha.gm-consultant@newscorp.com>
2026-04-30 13:04:27 +0530

    added top toolBar , dialog and toast feature ,splash screen and progress bar

--- app/src/main/java/com/example/news_app/MainActivity.kt
+++ app/src/main/java/com/example/news_app/MainActivity.kt
@@ -4,21 +4,36 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
+        //Keep splash screen visible until 2s
+        setupSplashScreen(splashScreen)
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
+        // Setting up the toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -37,5 +52,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun setupSplashScreen(splashScreen: SplashScreen) {
+
+        var isDataReady=false
+
+        lifecycleScope.launch {
+            delay(2000)
+            isDataReady = true
+        }
+             splashScreen.setKeepOnScreenCondition {
+                 !isDataReady
+             }
     }
 }
